115b3541d6eed8e885165ab9509b24e76c34b8fe
Vitalii Shved <vitalyashved@gmail.com>
2023-03-03 19:22:44 +0200

    add async redux

---
+++ Redux/src/redux/operations.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://6401fe9b3779a8626262a962.mockapi.io';
+
+export const fetchTasks = createAsyncThunk(
+  'tasks/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('tasks/');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addTask = createAsyncThunk(
+  'tasks/addTask',
+  async (text, thunkAPI) => {
+    try {
+      const response = await axios.post('/tasks', { text });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'tasks/deleteTasks',
+  async (taskId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/tasks/${taskId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const toggleCompleted = createAsyncThunk(
+  'tasks/toggleCompleted',
+  async (task, thunkAPI) => {
+    try {
+      const response = await PushSubscription(`/tasks/${task.Id}`, {
+        completed: !task.completed,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+// export const fetchTasks = () => async dispatch => {
+//   try {
+//     dispatch(fetchingInProgress());
+//     const response = await axios.get('/tasks');
+//     dispatch(fetchingSuccess(response.data));
+//   } catch (error) {
+//     dispatch(fetchingError(error.message));
+//   }
+// };
